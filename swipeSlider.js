f9e98d7eec8dfe0ad0e25e5ce35f8776b091171a
Hayden Wagner <haydenblainewagner@gmail.com>
2017-01-29 22:15:20 -0500

    More refactoring, very messy, working click event

--- swipeSlider.js
+++ swipeSlider.js
@@ -6,12 +6,34 @@
     }
 
     var SlideSwell = function( id ){
+        this.data = {};
         this.wrapperDiv = document.getElementById(id);
-        generateInnerDivs.call(this, this.wrapperDiv);
+        sanitizeContent.call(this);
+        generateInnerDivs.call(this);
+
+
+        setWidth.call(this);
     }
 
-    function generateInnerDivs(wrapperDiv){
-        var images = [].slice.call(wrapperDiv.children);
+    function sanitizeContent(){
+        var images = [].slice.call(this.wrapperDiv.children);
+        var self = this;
+        self.images = [];
+
+        images.map(function(i){
+            if(i.nodeName.toLowerCase() === 'img'){
+                i.setAttribute("class","swipeSlider_image");
+                self.images.push({el:i});
+                //movingDiv.appendChild(i);
+            }
+            else{
+                self.wrapperDiv.removeChild(i);
+            }
+        });
+    }
+
+    function generateInnerDivs(){
+        var self = this;
         var movingDiv = document.createElement('div'),
             arrowLeft = document.createElement('div'),
             arrowRight = document.createElement('div');
@@ -19,30 +41,231 @@
         movingDiv.setAttribute("class", "swipeSlider_image-div");
 
 
-        images.map(function(i){
-            if(i.nodeName.toLowerCase() === 'img'){
-                i.setAttribute("class","swipeSlider_image");
-                movingDiv.appendChild(i);
+        this.images.map(function(n){
+            if(n.el.nodeName.toLowerCase() === 'img'){
+                n.el.setAttribute("class","swipeSlider_image");
+                movingDiv.appendChild(n.el);
             }
         });
 
-        wrapperDiv.appendChild(movingDiv);
+        this.wrapperDiv.appendChild(movingDiv);
         this.movingDiv = movingDiv;
 
         arrowLeft.setAttribute('class', 'swipeSlider_arrow-div swipeSlider_arrow-div--left');
         arrowLeft.innerHTML = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="22.135px" height="34.01px" viewBox="1.575 1.432 22.135 34.01" enable-background="new 1.575 1.432 22.135 34.01" xml:space="preserve"><g><path fill="#FFFFFF" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M20.285,2.898c1.542-1.242,2.803-0.64,2.803,1.341v28.608c0,1.979-1.261,2.583-2.803,1.341L3.677,20.802c-1.541-1.242-1.541-3.275,0-4.519L20.285,2.898z"/></g></svg>';
-        wrapperDiv.appendChild(arrowLeft);
+
+        this.wrapperDiv.appendChild(arrowLeft);
         this.arrowLeft = arrowLeft;
+
+        arrowLeft.addEventListener("click", function( event ) {
+            event.stopPropagation();
+            self.arrowPress( "left" );
+        });
 
         arrowRight.setAttribute('class', 'swipeSlider_arrow-div swipeSlider_arrow-div--right');
         arrowRight.innerHTML = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="22.135px" height="34.01px" viewBox="1.575 1.432 22.135 34.01" enable-background="new 1.575 1.432 22.135 34.01" xml:space="preserve"><g><path fill="#FFFFFF" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M21.931,16.283c1.541,1.243,1.541,3.276,0,4.519L5.324,34.188c-1.542,1.242-2.803,0.639-2.803-1.341V4.239c0-1.98,1.261-2.583,2.803-1.341L21.931,16.283z"/></g></svg>';
-        wrapperDiv.appendChild(arrowRight);
+
+        this.wrapperDiv.appendChild(arrowRight);
         this.arrowRight = arrowRight;
+
+        arrowRight.addEventListener("click", function( event ) {
+            event.stopPropagation();
+            self.arrowPress( "right" );
+        });
     }
 
-    SlideSwell.prototype.method1 = function(){
+    function setWidth(){
+        var self = this;
+        var parentWidth = this.wrapperDiv.parentElement.clientWidth,
+            count = this.images.length,
+            imageWidthPerc = 0.90,
+            imageWidth = parentWidth * imageWidthPerc,
+            imagePadding = parentWidth * 0.025,
+            imageTotalWidth = imageWidth + imagePadding,
+            movingDivWidth = ( count * imageWidth ) + ( ( count ) * imagePadding )
+
+        this.data.halfImageTotal = imageTotalWidth / 2;
+
+        //imageDiv.id = swipeDiv.id;
+        //imageDiv.style.left = '0px';
+        this.movingDiv.style.width = movingDivWidth + "px";
+        this.movingDiv.style[ "padding-left" ] = ( ( 100 - ( 100 * imageWidthPerc ) ) / 2 ) + "%";
+
+        this.data.currentPos = 0; //change...statically set current image position...default set to zero
+
+        this.data.centeredPositions = [];
+        //for ( var a = 0; a < count; a++ ) {
+        this.images.map(function(n,i) {
+            self.data.centeredPositions.push(i * imageTotalWidth);
+            self.images[i].position = i * imageTotalWidth;
+
+            // var captionDiv = document.createElement( 'div' );
+            // captionDiv.className = "swipeSlider_captionDiv";
+            // captionDiv.style.width = ( imageWidth + imagePadding ) + "px";
+            //
+            // self.data[ key ].captionDivs[a] = captionDiv;
+
+            // var img = document.createElement( 'img' );
+            // img.className = "swipeSlider_image";
+            // img.id = swipeDiv.id;
+            // img.src = self.data[ key ].images[ a ].src;
+
+            self.images[i].el.style.width = imageWidth + "px";
+            self.images[i].el.style["margin-right"] = imagePadding + "px";
+
+            // var caption = document.createElement( 'p' );
+            // caption.className = "swipeSlider_caption";
+            //
+            // if ( self.data[ key ].images[ a ].caption ) {
+            //     caption.innerHTML = self.data[ key ].images[ a ].caption;
+            // }
+            //
+            // self.data[ key ].images[a].captionElement = caption;
 
 
+            // captionDiv.appendChild( img );
+            // captionDiv.appendChild( caption );
+            // imageDiv.appendChild( captionDiv );
+
+            self.data.measureImg = self.images[i].el;
+        });
+
+        this.data.measureImg.addEventListener("load", function(){
+            //var badSingleImgEl = document.querySelector('.swipeSlider_image');
+
+            var natH = this.naturalHeight;
+            var natW = this.naturalWidth;
+
+            self.data.natH = natH;
+            self.data.natW = natW;
+
+            var setHeight = ( imageWidth * natH ) / natW;
+            self.data.imageWidth = imageWidth;
+            //swipeDiv.style.height = ( setHeight + 10) + "px";
+            //swipeDiv.setAttribute( "height", ( setHeight + 10) + "px" );
+
+            //bad to select like this, should have ref to outer div saved to reference here
+            self.wrapperDiv.style.height = setHeight + 10 + 'px';
+            //does not need to be set w/ javascript if it is always the same
+            self.wrapperDiv.style.paddingBottom = '30px';
+        });
+
+            //this.setSwipeListeners( swipeDiv );
+
+            // swipeDiv.appendChild( imageDiv );
+            // swipeDiv.appendChild( swipeSlider.addArrows( "left", key ) );
+            // swipeDiv.appendChild( swipeSlider.addArrows( "right", key ) );
+
+        this.changeHeight( true );
+        //this.checkArrowVis( key );
+
+
+
+    }
+
+    // function changeHeight( start ) {
+    //     if ( start != undefined ) {
+    //         obj.images[ obj.currentPos ].element.className = "swipeSlider_image--selected";
+    //         obj.images[ obj.currentPos ].captionElement.style.opacity = 1;
+    //     } else {
+    //         obj.images[ obj.currentPos ].element.className = "swipeSlider_image--selected";
+    //         obj.images[ obj.currentPos ].captionElement.style.opacity = 1;
+    //         obj.images[ obj.pastPosition ].element.className = "swipeSlider_image";
+    //         obj.images[ obj.pastPosition ].captionElement.style.opacity = 0;
+    //
+    //     }
+    // }
+
+
+    SlideSwell.prototype.changeHeight = function(start){
+        if ( start != undefined ) {
+            this.images[ this.data.currentPos ].el.className = "swipeSlider_image--selected";
+            //this.images[ this.currentPos ].captionElement.style.opacity = 1;
+        } else {
+            this.images[ this.data.currentPos ].el.className = "swipeSlider_image--selected";
+            //this.images[ this.currentPos ].captionElement.style.opacity = 1;
+            this.images[ this.data.pastPosition ].el.className = "swipeSlider_image";
+            //this.images[ this.pastPosition ].captionElement.style.opacity = 0;
+        }
+    }
+
+
+    SlideSwell.prototype.arrowPress = function(dir){
+        console.log(this);
+        console.log(dir);
+
+        //var id;
+        var sD = this.data,
+            pos = sD.currentPos;
+
+        //if ( !this.lastTouchedId ) {
+        //id = el.parentElement.id;
+       // this.lastTouchedId = id;
+        //} else {
+        // id = this.lastTouchedId;
+        // imgDiv = this.data[id].el;
+        //}
+
+        //could clean this up a lot
+        if ( dir === "right" ) {
+            pos++;
+            if ( sD.centeredPositions[ pos ] !== undefined ) {
+                swipeSlider.cycle( this.movingDiv, sD.centeredPositions[pos]);
+
+                if ( sD.centeredPositions[pos] === 0 ) {
+                    sD.xCurrent = sD.centeredPositions[pos];
+                    sD.xUpdate = sD.centeredPositions[pos];
+                } else {
+                    sD.xCurrent = -sD.centeredPositions[pos];
+                    sD.xUpdate = -sD.centeredPositions[pos];
+                }
+
+                sD.pastPosition = sD.currentPos;
+                sD.currentPos = pos;
+                this.checkArrowVis();
+
+                this.changeHeight();
+            } else {
+                pos--;
+            }
+        }
+
+        if ( dir === "left" ) {
+            pos--;
+            if ( sD.centeredPositions[ pos ] !== undefined ) {
+                swipeSlider.cycle( this.movingDiv, sD.centeredPositions[pos]);
+
+                if ( sD.centeredPositions[pos] === 0 ) {
+                    sD.xCurrent = sD.centeredPositions[pos];
+                    sD.xUpdate = sD.centeredPositions[pos];
+                } else {
+                    sD.xCurrent = -sD.centeredPositions[pos];
+                    sD.xUpdate = -sD.centeredPositions[pos];
+                }
+
+                sD.pastPosition = sD.currentPos;
+                sD.currentPos = pos;
+                this.checkArrowVis();
+
+                this.changeHeight();
+            } else {
+                pos++;
+            }
+        }
+    }
+
+    SlideSwell.prototype.checkArrowVis = function() {
+        if ( this.data.currentPos === 0 ) {
+            this.arrowLeft.style.display = 'none';
+        } else {
+            this.arrowLeft.style.display = 'block';
+        }
+
+        if ( this.data.currentPos === this.images.length - 1 ) {
+            this.arrowRight.style.display = 'none';
+        } else {
+            this.arrowRight.style.display = 'block';
+        }
     }
 
 
@@ -386,7 +609,7 @@
           }
         }
         */
-       if ( start !== undefined ) {
+       if ( start != undefined ) {
         obj.images[ obj.currentPos ].element.className = "swipeSlider_image--selected";
         obj.images[ obj.currentPos ].captionElement.style.opacity = 1;
        } else {
@@ -504,7 +727,7 @@
         if ( dir === "right" ) {
           pos++;
           if ( sD.centeredPositions[ pos ] !== undefined ) {
-            swipeSlider.cycle( imgDiv, sD.centeredPositions[pos], id);
+            swipeSlider.cycle( imgDiv, sD.centeredPositions[pos]);
 
             if ( sD.centeredPositions[pos] === 0 ) {
               sD.xCurrent = sD.centeredPositions[pos];
@@ -527,7 +750,7 @@
         if ( dir === "left" ) {
           pos--;
           if ( sD.centeredPositions[ pos ] !== undefined ) {
-            swipeSlider.cycle( imgDiv, sD.centeredPositions[pos], id);
+            swipeSlider.cycle( imgDiv, sD.centeredPositions[pos]);
 
             if ( sD.centeredPositions[pos] === 0 ) {
               sD.xCurrent = sD.centeredPositions[pos];
@@ -553,7 +776,7 @@
 
       //make it so the move param has the negative or positive,
         //get rid of negative sign bullshit
-      cycle: function ( element, move, key ) {
+      cycle: function ( element, move) {
         // $( imgDiv ).stop().animate( {
         //   left: -move + "px"
         // }, 250 );
@@ -698,15 +921,15 @@
         //if everything else is good
       checkArrowVis: function( key ) {
         if ( swipeSlider.data[ key ].currentPos === 0 ) {
-            swipeSlider.data[ key ].arrowLeft.style.opacity = '0.0';
+            swipeSlider.data[ key ].arrowLeft.style.display = 'none';
         } else {
-            swipeSlider.data[ key ].arrowLeft.style.opacity = '1.0';
+            swipeSlider.data[ key ].arrowLeft.style.display = 'block';
         }
 
         if ( swipeSlider.data[ key ].currentPos === swipeSlider.data[ key ].count - 1 ) {
-            swipeSlider.data[ key ].arrowRight.style.opacity = '0.0';
+            swipeSlider.data[ key ].arrowRight.style.display = 'none';
         } else {
-            swipeSlider.data[ key ].arrowRight.style.opacity = '1.0';
+            swipeSlider.data[ key ].arrowRight.style.display = 'block';
         }
       }
 
