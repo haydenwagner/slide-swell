fdeba80cb93947d1d81f6854d8d5fc81da64f996
Hayden Wagner <haydenblainewagner@gmail.com>
2017-04-18 11:29:43 -0400

    Adds methods to slideswell obj, can advance/return position

--- sw.js
+++ sw.js
@@ -1,37 +1,89 @@
 var SlideSwell = {
 
-    init: function(id){
+    init: function(id, options){
+        this.options = options || {};
         this.domElement = document.getElementById(id);
         this.slides = [];
         Array.prototype.slice.call(this.domElement.querySelectorAll('img')).map(function(x){
             this.slides.push(Object.create(Slide).init(x));
         }, this);
-        this.slideContainer = Object.create(WrapperDiv).init(this.slides);
+        this.wrapperDiv = Object.create(WrapperDiv).init(this);
+        this.currentPosition = 0;
+
+        this.setStartPosition();
+
+        //tests
+        this.advancePosition();
+        this.advancePosition();
+        this.returnPosition();
+        this.returnPosition();
+    },
+
+    setStartPosition: function(){
+        if(this.options.start){
+            if(this.options.start <= this.slides.length-1){
+                this.setPosition(this.options.start);
+            }
+            else{
+                throw new Error('Starting position is invalid, it does not match the available slides');
+            }
+        }
+        else{
+            this.setPosition(0);
+        }
+    },
+
+    setPosition:function(position){
+        var slideOffset = this.slides[position].getCenterOffset(),
+            sliderWidth = this.domElement.clientWidth,
+            newOffset = slideOffset - sliderWidth/2;
+
+        this.wrapperDiv.changeOffset(newOffset);
+        this.currentPosition = position;
+    },
+
+    advancePosition: function(){
+        if( (this.currentPosition + 1) <= this.slides.length - 1 ){
+            this.setPosition(this.currentPosition + 1);
+        }
+    },
+
+    returnPosition: function(){
+        if( (this.currentPosition - 1) >= 0 ){
+            this.setPosition(this.currentPosition - 1);
+        }
     }
 };
 
 var WrapperDiv = {
-    init: function(slideArr){
-        return this.wrapImages(slideArr);
-    },
-
-    wrapImages: function(slideArr){
+    init: function(parent){
         var wrapperDiv = document.createElement('div');
         this.domElement = wrapperDiv;
-        slideArr.map(function(x){
+        this.domElement.className = 'slideswell2_image-div';
+        this.wrapImages(wrapperDiv, parent);
+        return this;
+    },
+
+    wrapImages: function(wrapperDiv, parent){
+        parent.slides.map(function(x){
             wrapperDiv.append(x.domElement);
         });
-        return this;
+        parent.domElement.append(wrapperDiv);
+    },
+    
+    changeOffset: function(newOffset){
+        this.domElement.style.left = -newOffset + 'px';
     }
 };
 
 var Slide = {
     init: function(imgEl){
         this.domElement = imgEl;
+        this.domElement.className = 'slideswell2_image';
         return this;
     },
 
     getCenterOffset: function(wrapper){
-        return this.domElement.offsetLeft + this.domElement.style.width/2;
+        return this.domElement.offsetLeft + this.domElement.width/2;
     }
 };
